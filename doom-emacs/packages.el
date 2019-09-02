88807019774a811bff31d63fa2af82fe48edaae1
PoiScript <poiscript@gmail.com>
2019-09-02 16:56:42 +0800

    chore: rename .doom.d/ to doom-emacs/

--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -1,11 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-(package! cargo)
 (package! cdlatex)
-(package! company-lsp)
 (package! fcitx)
-(package! lsp-mode)
-(package! lsp-ui)
-(package! memory-usage)
 (package! tiny)
