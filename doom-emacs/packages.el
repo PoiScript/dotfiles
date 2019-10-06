1851aeefacaa753de451cb864d945d516505caa5
PoiScript <poiscript@gmail.com>
2019-10-07 00:01:41 +0800

    doom: org-drill configuration

--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -1,7 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
+(package! auctex)
 (package! cdlatex)
 (package! drag-stuff)
+(package! edit-indirect)
 (package! fcitx)
+;; Sometimes, general.el don't get loaded automatically at startup.
+(package! general)
+(package! org-drill :recipe (:host gitlab :repo "phillord/org-drill"))
 (package! tiny)
